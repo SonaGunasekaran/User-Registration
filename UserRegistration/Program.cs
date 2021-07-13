2fe8aba14877f44ede709c733cb70fbf548f6c34
[Sona] <[sona16061999@gmail.com]>
2021-07-13 23:16:52 +0530

    [Sona] LastName is validated using regex pattern

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -10,7 +10,9 @@ namespace UserRegistration
             Console.Write("Enter your first name:");
             string firstName = Console.ReadLine();
             user.Check(firstName);
-            
+            Console.Write("Enter your last name:");
+            string lastName = Console.ReadLine();
+            user.Check(lastName);
 
         }
     }
