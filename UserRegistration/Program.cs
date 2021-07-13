3a511d1de790f2cd2937ccec0a870634f9563f04
[Sona] <[sona16061999@gmail.com]>
2021-07-13 23:11:18 +0530

    [Sona] FirstName is validated using regex pattern

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -6,7 +6,12 @@ namespace UserRegistration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("User Registration!");
+            Registration user = new Registration();
+            Console.Write("Enter your first name:");
+            string firstName = Console.ReadLine();
+            user.Check(firstName);
+            
+
         }
     }
 }
