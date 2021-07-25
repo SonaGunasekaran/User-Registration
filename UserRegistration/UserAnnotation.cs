9c667e75eb69fcdd616e620d14e0a6060bbb8ed3
[Sona] <[sona16061999@gmail.com]>
2021-07-25 19:33:22 +0530

    [Sona] Check for Validity an return happy or sad

--- UserRegistration/UserAnnotation.cs
+++ UserRegistration/UserAnnotation.cs
@@ -13,13 +13,11 @@ namespace UserRegistration
         [DataType(DataType.Text)]
         public string firstName { get; set; }
 
-        
         [Required(ErrorMessage = "{0} is required")]
         [StringLength(50, MinimumLength = 3, ErrorMessage = "Last Name should be min of 3")]
         [DataType(DataType.Text)]
         public string lastName { get; set; }
 
-        
         [Required(ErrorMessage = "{0} is required")]
         [DataType(DataType.PhoneNumber)]
         [Phone]
--- UserRegistrationTest/UnitTest1.cs
+++ UserRegistrationTest/UnitTest1.cs
@@ -6,59 +6,51 @@ namespace UserRegistrationTest
     [TestClass]
     public class UnitTest1
     {
-       UserRegistrationExp registration;
-       UserAnnotation user;
+        UserRegistrationExp registration;
+        UserAnnotation user;
         [TestInitialize]
         public void TestSetup()
         {
             registration = new UserRegistrationExp();
             user = new UserAnnotation();
-
         }
         [TestCategory("Name")]
         [TestMethod]
         public void FirstNameException()
         {
-            //Assign
             string expected = "Name should not be empty";
             string name = "";
             try
             {
-                //act
                 string actual = registration.CheckName(name);
                 Assert.AreEqual(actual, expected);
             }
             catch (CustomException ex)
             {
-                //assert
                 Assert.AreEqual(ex.Message, expected);
             }
         }
         [TestMethod]
         public void LastNameException()
         {
-            //Assign
             string expected = "Name should not be null";
             string name = null;
             try
             {
-                //act
                 string actual = registration.CheckName(name);
                 Assert.AreEqual(actual, expected);
             }
             catch (CustomException ex)
             {
-                //assert
+
                 Assert.AreEqual(ex.Message, expected);
             }
         }
         [TestMethod]
         public void PasswordExceptionTest()
         {
-            //Assign
             string expected = "Password should not be empty";
             string password = "";
-            //act
             try
             {
                 string actual = registration.PasswordCheck(password);
@@ -66,7 +58,7 @@ namespace UserRegistrationTest
             }
             catch (CustomException ce)
             {
-                //assert
+                
                 Assert.AreEqual(ce.Message, expected);
             }
         }
@@ -79,12 +71,9 @@ namespace UserRegistrationTest
             user.EmailAddress = "dstvd@gmail.com";
             user.Password = "Dasa@1mjh";
             string expected = "First Name should be min of 3";
-            string actual = UserRegistrationExp.TestUserRegistration(user);
+            string actual = UserRegistrationExp.ValidateUserRegistration(user);
             Assert.AreEqual(expected, actual);
         }
-        /// <summary>
-        /// Checks for validation using annotation = if phone number not valid
-        /// </summary>
         [TestMethod]
         public void CheckValidation2()
         {
@@ -94,7 +83,7 @@ namespace UserRegistrationTest
             user.EmailAddress = "dstvd@gmail.com";
             user.Password = "Dasa@1mjh";
             string expected = "Phone number should exactly 10";
-            string actual = UserRegistrationExp.TestUserRegistration(user);
+            string actual = UserRegistrationExp.ValidateUserRegistration(user);
             Assert.AreEqual(expected, actual);
         }
         [TestMethod]
@@ -106,7 +95,31 @@ namespace UserRegistrationTest
             user.EmailAddress = "dstvd@gmail.com";
             user.Password = "Dasa@1mjh";
             string expected = "Last Name should be min of 3";
-            string actual = UserRegistrationExp.TestUserRegistration(user);
+            string actual = UserRegistrationExp.ValidateUserRegistration(user);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void CheckValiditionShouldReturnHappy()
+        {
+            user.firstName = "Damon";
+            user.lastName = "Salvatore";
+            user.phoneNumber = "4567892378";
+            user.EmailAddress = "dstvd@gmail.com";
+            user.Password = "Dasa@1mjh";
+            string expected = "Happy";
+            string actual = UserRegistrationExp.ValidateUserRegistration(user);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void CheckValiditionShouldReturnSad()
+        {
+            user.firstName = "Damon";
+            user.lastName = "S";
+            user.phoneNumber = "45678923";
+            user.EmailAddress = "dstvd@gmail.com";
+            user.Password = "Dasa@1mjh";
+            string expected = "Sad";
+            string actual = UserRegistrationExp.ValidateUserRegistration(user);
             Assert.AreEqual(expected, actual);
         }
     }
